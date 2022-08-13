07469b8623026c52f47ff9a80cdfbc171898c242
TravisRo <libusbdotnet@gmail.com>
2022-08-13 17:17:53 -0600

    Change CDC RX/TX buffer sizes from 64 to 256

--- tusb_config.h
+++ tusb_config.h
@@ -107,8 +107,8 @@ extern "C" {
 
 
 // CDC FIFO size of TX and RX
-#define CFG_TUD_CDC_RX_BUFSIZE   (64)
-#define CFG_TUD_CDC_TX_BUFSIZE   (64)
+#define CFG_TUD_CDC_RX_BUFSIZE   (256)
+#define CFG_TUD_CDC_TX_BUFSIZE   (256)
 
 // CDC Endpoint transfer buffer size, more is faster
 #define CFG_TUD_CDC_EP_BUFSIZE   (64)
