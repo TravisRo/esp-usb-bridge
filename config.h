3cd5bcd6f21a06dd35dff13514ba76a5ad509dba
TravisRo <libusbdotnet@gmail.com>
2022-08-13 21:50:15 -0600

    Normalize and simplify pin naming conventions

--- config.h
+++ config.h
@@ -3,34 +3,37 @@
 #include <assert.h>
 #include "bsp/board.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+/*
+ * PICO_DEFAULT_WS2812_PIN is generally defined in the boards header file for those that have it.
+ * You can specify your board at the top of the parent CMakeList.txt file. 
+ * EG: set(PICO_BOARD tzt_rgb_usbc_rp2040)
+ * 
+ * The board header files are located in the pico-sdk in the folder pico-sdk/src/boards/include/boards
+ * 
+ * If you have a custom board with a WS2812 you can define WS2812_PIN here.
+ */
 
-// PICO_DEFAULT_WS2812_PIN is generally defined in the boards header file for those that have it.
-// If you have a custom board with a WS2812 you can define WS2812_PIN here.
 #ifdef PICO_DEFAULT_WS2812_PIN
 #define WS2812_PIN PICO_DEFAULT_WS2812_PIN
 #endif
 	
-// pins connected to esp32 target
+// pins connected to esp32 target for the serial programming interface
 #define GPIO_BOOT (6)
 #define GPIO_RST (7)
 #define GPIO_TXD (4)
 #define GPIO_RXD (5)
 
-#define CONFIG_BRIDGE_GPIO_TCK (29)
-#define CONFIG_BRIDGE_GPIO_TDI (27)
-#define CONFIG_BRIDGE_GPIO_TMS (28)
-#define CONFIG_BRIDGE_GPIO_TDO (14)
+// pins connected to esp32 target for the JTAG debugging interface
+#define GPIO_TCK (29)
+#define GPIO_TDI (27)
+#define GPIO_TMS (28)
+#define GPIO_TDO (14)
 
-_Static_assert(CONFIG_BRIDGE_GPIO_TMS == CONFIG_BRIDGE_GPIO_TDI+1, "TDI and TMS pins must be sequential! EG: If TDI=27 then TMS must be 28");
+_Static_assert(GPIO_TMS == GPIO_TDI+1, "TDI and TMS pins must be sequential! EG: If TDI=27 then TMS must be 28");
 
 #define SLAVE_UART_NUM          uart1
 #define SLAVE_UART_BUF_SIZE     (2 * 1024)
 #define SLAVE_UART_DEFAULT_BAUD 115200
-
-#define USB_SEND_RINGBUFFER_SIZE SLAVE_UART_BUF_SIZE
 
 #define CONFIG_BRIDGE_MSC_VOLUME_LABEL "ESPPROG_MSC"
 
@@ -58,6 +61,5 @@ typedef enum
 #define STACK_SIZE_FROM_BYTES(stackSizeInBytes) ((stackSizeInBytes)/sizeof(StackType_t))
 
 #define eub_abort() do{ printf("eub_abort!!\r\n"); }while(1)
-#ifdef __cplusplus
-}
+
 #endif
